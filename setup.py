c2dafc485ba9ee530f6161cf1c982f6bb1f07872
PRATHAMESH SUHAS URAVANE <upratham2002@gmail.com>
2026-03-07 21:03:58 -0500

    Project structure created

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Prathamesh Uravane",
+    author_email="upratham2002@gmail.com",
+    packages=find_packages()
+)
