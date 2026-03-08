80f58ae19c11502cd2830fbbf87ac1383ee0be53
PRATHAMESH SUHAS URAVANE <upratham2002@gmail.com>
2026-03-07 22:00:31 -0500

    venv set

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="src",
     version="0.0.1",
-    author="Prathamesh Uravane",
-    author_email="upratham2002@gmail.com",
+    author="Prathamesh Uravane, Sankeerth B, Claude B",
+    author_email="upratham2002@gmail.com, sankeerthb@gmail.com, claudesaul@outlook.com",
+    description="A Vehicle Maintenance Prediction for vehicle owners",
     packages=find_packages()
 )
